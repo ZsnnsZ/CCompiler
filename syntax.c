fe6ea5fb7c8e0ff81f95f2bad43e74b4cf1f026c
ZsnnsZ <925453683@qq.com>
2016-12-23 10:34:56 +0800

    Update syntax.c

--- syntax.c
+++ syntax.c
@@ -1,21 +1,20 @@
 #include "word.h"
 #include "syntax.h"
 
-char input[255];//输入缓冲区
-char token[55] = "";//单词缓冲区
-int p_input;//输入换缓冲区指针
-int p_token;//单词缓冲区指针
-int flag = 0;//1表示刚读取一个变量或常数，"+/-"为运算符;0反之，"+/-"可能为数值符号
-char ch;//当前读入字符
+char input[255];// 输入缓冲区
+char token[55] = "";// 单词缓冲区
+int p_input;// 输入缓冲区指针
+int p_token;// 单词缓冲区指针
+int flag = 0;// 1表示刚读取一个变量或常数，"+/-"为运算符;0反之，"+/-"可能为数值符号
+char ch;// 当前读入字符
 char *rwtab[10] = {"main", "int", "float", "double", "char", "if", "else", "do", "while", _KEY_WORD_END};
-double sum;//存放数值
-int input_f;//用于判断oneword是否要输出
+double sum;// 存放数值
+int input_f;// 用于判断oneword是否要输出
 
-int type;//存放当前的typenum
+int type;// 存放当前的typenum
 
 int successflag = 0;
 int failflag = 0;
-int error_f = 0;
 
 void main()
 {
@@ -25,6 +24,7 @@ void main()
 	scanf("%[^#]s", input);
 	printf("\n");
 	p_input = 0;
+	flag = 0;
 	pre_do();
 	printf("Your words after pre_do:\n%s\n\n", input);
 	printf("The results:\n");
@@ -35,31 +35,38 @@ void main()
 			printf("(%d , %s)\n", oneword->typenum, oneword->word);
 			over = oneword->typenum;
 	}
+	// 调用语法分析程序
+	anlyse_syntax();
+}
 
-	//语法分析
+//语法分析程序
+void anlyse_syntax()
+{
 	p_input = 0;
-	flag = 0;
 	printf("\nbegin anlysing the syntax:\n");
 	sscaner();
-	expression();
-
+	// statement_strand();
+	program();
+	//if_statement();
+	//do_statement();
 	if(type == 1000)
 		successflag = 1;
 	else
+	{
+		// printf("\nanlyse unexpected ending\n");
+		// error();
 		failflag = 1;
-
+	}
 	if(failflag == 1)
 	{
-		if(!error_f)
-			printf("\nunexpected character\n");
 		printf("\nfail in anlysing syntax\n");
 	}
 
 	if(successflag == 1 && failflag == 0)
 		printf("\nsuccess in anlysing syntax\n");
-
 }
 
+//扫描
 void sscaner()
 {
 	WORD *sword = NULL;
@@ -71,7 +78,243 @@ void sscaner()
 	printf("current typenum: %d\n", type);
 }
 
-void expression()//<表达式> ::= <项>{ +<项>|-<项>}
+//出错处理
+void error()
+{
+	printf("Syntax error around %d\n\n",type);
+}
+
+//<程序> ::= main()<语句块>
+void program()
+{
+	if(type == 1)
+	{
+		sscaner();
+		if(type == 26)
+		{
+			sscaner();
+			if(type == 27)
+			{
+				sscaner();
+				block();
+			}
+			else
+			{
+				printf("\nprogram(): main() lack of ')'\n");
+				error();
+			}
+		}
+		else
+		{
+			printf("\nprograme(): main() lack of '('\n");
+			error();
+		}
+	}
+	else
+	{
+		printf("\nprograme(): main() lack of 'main'\n");
+		error();
+	}
+}
+
+//<语句块> ::= ‘{‘<语句串>’}’
+void block()
+{
+	if(type == 28)
+	{
+		sscaner();
+		statement_strand();
+		if(type == 29)
+			sscaner();
+		else
+		{
+			printf("\nblock(): lack of '}'\n");
+			error();
+		}
+	}
+	else
+	{
+		printf("\nblock(): lack of '{'\n");
+		error();
+	}
+}
+
+//<语句串>::=<语句>{;<语句>}
+void statement_strand()
+{
+	statement();
+	if(type == 31)
+		sscaner();
+	else
+	{
+		printf("\nstatement_strand()1: lack of ';' or ';' is not necessary\n");
+		error();
+	}
+	while(type != 29)
+	{
+		statement();
+		if(type == 31)
+			sscaner();
+		else
+		{
+			printf("\nstatement_strand()2: lack of ';'\n");
+			error();
+		}
+	}
+	return;
+}
+
+//<语句>::=<赋值语句>|<条件语句>|<循环语句>
+void statement()
+{
+	if(type == 10)
+		assign_statement();
+	else if(type == 6)
+		if_statement();
+	else if(type == 8)
+		do_statement();
+	else
+	{
+		printf("\nstatement(): unexpected statement\n");
+		error();
+	}
+	return;
+}
+
+//<赋值语句>::=ID=<表达式>
+void assign_statement()
+{
+	if(type == 10)
+	{
+		sscaner();
+		if(type == 21)
+		{
+			sscaner();
+			expression();
+		}
+		else
+		{
+			printf("\nassign_statement(): lack of '='\n");
+			error();
+		}
+	}
+	else
+	{
+		printf("\nassign_statement(): lack of 'ID'\n");
+		error();
+	}
+	return;
+}
+
+//<条件语句>::=if<条件><语句块>[else <语句块>]
+//<条件语句>::=if'('<条件>')'<语句块>[else <语句块>]
+void if_statement()
+{
+	if(type == 6)
+	{
+		sscaner();
+		if(type == 26)
+		{
+			sscaner();
+			condition();
+			if(type == 27)
+			{
+				sscaner();
+				block();
+				if(type == 7)
+				{
+					sscaner();
+					block();
+				}
+			}
+			else
+			{
+				printf("\nif_statement(): lack of ')'\n");
+				error();
+			}
+		}
+		else
+		{
+			printf("\nif_statement(): lack of '('\n");
+			error();
+		}
+	}
+	else
+	{
+		printf("\nif_statement(): lack of 'if'\n");
+		error();
+	}
+	return;
+}
+
+//<条件>::=<表达式><关系运算符><表达式>
+void condition()
+{
+	expression();
+	operator();
+	expression();
+	return;
+}
+
+//<关系运算符> ::= <|<=|>|>=|==|!=
+void operator()
+{
+	if(type == 32 || type == 33 || type == 34 || type == 35 || type == 36 || type == 37)
+		sscaner();
+	else
+	{
+		printf("\noperator(): unexpected operator\n");
+		error();
+	}
+	return;
+}
+
+//<循环语句>::=do <语句块>while <条件>
+void do_statement()
+{
+	if(type == 8)
+	{
+		sscaner();
+		block();
+		if(type == 9)
+		{
+			sscaner();
+			if(type == 26)
+			{
+				sscaner();
+				condition();
+				if(type == 27)
+				{
+					sscaner();
+				}
+				else
+				{
+					printf("\ndo_statement(): lack of ')'\n");
+					error();
+				}
+			}
+			else
+			{
+				printf("\ndo_statement(): lack of '('\n");
+				error();
+			}
+		}
+		else
+		{
+			printf("\ndo_statement(): lack of 'while'\n");
+			error();
+		}
+	}
+	else
+	{	
+		printf("\ndo_statement(): lack of 'do'\n");
+		error();
+	}
+	return;
+}
+
+//<表达式> ::= <项>{+<项>|-<项>}
+void expression()
 {
     term();
     while(type == 22 || type == 23)
@@ -82,7 +325,8 @@ void expression()//<表达式> ::= <项>{ +<项>|-<项>}
     return;
 }
 
-void term()//<项> ::= <因子>{*<因子>|/<因子>}
+//<项> ::= <因子>{*<因子>|/<因子>}
+void term()
 {
     factor();
     while(type == 24 || type == 25)
@@ -93,9 +337,12 @@ void term()//<项> ::= <因子>{*<因子>|/<因子>}
     return;
 }
 
-void factor()//<因子> ::=ID|num|(<表达式>)
+//<因子> ::=ID|num|(<表达式>)
+void factor()
 {
-    if (type == 26)//'('
+    if(type == 10 || type == 20)//ID|num
+    	sscaner();
+    else if (type == 26)//'('
     {
         sscaner();
         expression();
@@ -104,16 +351,14 @@ void factor()//<因子> ::=ID|num|(<表达式>)
         else 
         { 
             failflag = 1;
-            printf("\nlack of ')'\n");
-            error_f = 1;
+            printf("\nfactor(): lack of ')'\n");
+            error();
         }
     }
-    else if((type == 10) || (type == 20))//ID|num
-    	sscaner();
     else { 
         failflag = 1;
-        printf("\nexpression error\n");
-        error_f = 1;
+        printf("\nfator(): expression error\n");
+        error();
     }
     return;
 }
@@ -399,7 +644,7 @@ WORD *scaner(){
 				else
 					{
 						input_f = 0;
-						p_input++;
+						p_input = p_input + 2;
 					}
 			}
 			retract();
